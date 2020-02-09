475e40287fb9945dd64fbedf5e5b85de95bbc388
Ryo Yasumori <kylasakerskog26@gmail.com>
2020-02-09 17:03:52 +0900

    WIP

--- pysh.py
+++ pysh.py
@@ -25,66 +25,65 @@ def is_built_in(args):
 
 def split_proc(in_args):
     # パイプを分割する処理
-    in_piped = []
     elements = []
     skippable = False
     redir_in = ''
-    redir_out = ''
-    redir_out_add = ''
+    redir_out = []
+    redir_out_add = []
     counts = 0
     
     for i, el in enumerate(in_args):
         if skippable == True:
             skippable = False
-            # continue
         elif el in ['<', '>', '>>']:
             if (i+1) < len(in_args):
                 skippable = True
                 if el == '<':
                     redir_in = in_args[i+1]
-                    sys.stderr.write('redir_in: ' + redir_in + '\n')
+                    # sys.stderr.write('redir_in: ' + redir_in + '\n')
                 elif el == '>':
-                    redir_out = in_args[i+1]
-                    sys.stderr.write('redir_out: ' + redir_out + '\n')
+                    redir_out.append(in_args[i+1])
+                    # sys.stderr.write('redir_out: ' + redir_out + '\n')
                 else:
-                    redir_out_add = in_args[i+1]
-                    sys.stderr.write('redir_out_add: ' + redir_out_add + '\n')
+                    redir_out_add.append(in_args[i+1])
+                    # sys.stderr.write('redir_out_add: ' + redir_out_add + '\n')
             else:
                 sys.stderr.write('-pysh: syntax error: ' + in_args[i] + '\n')
-                continue
         elif el == '|':
-            in_piped.append(elements)
-            sys.stderr.write('element' + str(counts) + ' : '  + str(in_piped[counts]) + '\n\n')
-            counts += 1
+            exec_sh(elements)
+            # sys.stderr.write('element' + str(counts) + ' : '  + str(in_piped[counts]) + '\n\n')
+            # counts += 1
             elements = []
+            redir_in = ''
+            redir_out = []
+            redir_out_add = []
         else:
             elements.append(el)
-
-    if elements != []:
-        in_piped.append(elements)
-        sys.stderr.write('element' + str(counts) + ' : '  + str(in_piped[counts]) + '\n\n')
-                
             
+    if elements != []:
+        exec_sh(elements)
+        # sys.stderr.write('element' + str(counts) + ' : '  + str(in_piped[counts]) + '\n\n')
+
+def exec_sh(args):
+    # parent_pid =  os.getpid()
+    child_pid = os.fork()
+    
+    # os.fork()は子プロセスのときは0, それ以外はchild_pidを返す
+    if child_pid == 0:
+        try:
+            os.execvp(args[0], args)
+        except:
+            sys.stderr.write('-pysh: command not found: ' + args[0] + '\n')
+    else:
+        os.waitpid(child_pid, 0)
+
 if __name__ == "__main__":
     while True:
         sys.stderr.write('mysh$ ')
         cmd = input()
         args = cmd.split()
 
-        split_proc(args)
-        """
         if is_built_in(args):
             continue
-        # parent_pid =  os.getpid()
-        child_pid = os.fork()
 
-        # os.fork()は子プロセスのときは0, それ以外はchild_pidを返す
-        if child_pid == 0:
-            try:
-                os.execvp(args[0], args)
-            except:
-                sys.stderr.write('-pysh: command not found: ' + args[0] + '\n')
-        else:
-            os.waitpid(child_pid, 0)
-                
-        """
+        exec_sh(args)
